114066f853f43816083205a447474c1952ae2c53
Moritz <smappetizer@gmail.com>
2023-02-15 16:14:07 +0100

    Add API fetch call

--- netlify/functions/api-call.js
+++ netlify/functions/api-call.js
@@ -1,12 +1,13 @@
 exports.handler = async function(event, context) {
-    // your server-side functionality
-    console.log({event})
-    console.log({context})
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            event: event,
-            context: context
-        }),
-      };
-};
+    fetch("/https://platform.smapone.com/Backend/v1/Smaps/6dc3a428-076f-4fd9-b257-c5cf94a78ed5/Data?accessToken=b87e89f3372bbedebf5d2175ab416f1f2bdd7613")
+    .then((response) => response.json())
+    .then((data) => {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                event: event,
+                context: context
+            })
+        }
+    })
+}
