c311d89b526f59cf61b489df8664fbc6629b1203
Moritz <smappetizer@gmail.com>
2023-02-15 12:52:16 +0100

    Error fix attempt

--- netlify/functions/api-call.js
+++ netlify/functions/api-call.js
@@ -1,5 +1,12 @@
-exports.handler = async function (event, context) {
+exports.handler = async function(event, context) {
     // your server-side functionality
     console.log({event})
     console.log({context})
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            event: event,
+            context: context
+        }),
+      };
 };
