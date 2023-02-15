ca9afdbfc1bc7afb95bf56405574d7dae6c1b166
Moritz <smappetizer@gmail.com>
2023-02-15 16:19:01 +0100

    Re-remove colons and fix error instead

--- netlify/functions/api-call.js
+++ netlify/functions/api-call.js
@@ -1,7 +1,7 @@
 exports.handler = async function(event, context) {
-    const apiToken = process.env.API_TOKEN;
+    const apiToken = process.env.API_TOKEN
 
-    fetch("/https://platform.smapone.com/Backend/v1/Smaps/6dc3a428-076f-4fd9-b257-c5cf94a78ed5/Data?accessToken=" + apiToken)
+    fetch("https://platform.smapone.com/Backend/v1/Smaps/6dc3a428-076f-4fd9-b257-c5cf94a78ed5/Data?accessToken=" + apiToken)
         .then((response) => response.json())
         .then((data) => {
             return {
@@ -10,6 +10,6 @@ exports.handler = async function(event, context) {
                     event: event,
                     context: context
                 })
-            };
-        });
+            }
+        })
 }
