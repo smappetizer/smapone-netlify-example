6da1a31c5357f57c0ae17c32ccc7181a8690deb2
Moritz <smappetizer@gmail.com>
2023-02-15 16:17:57 +0100

    Add semicolons

--- netlify/functions/api-call.js
+++ netlify/functions/api-call.js
@@ -1,15 +1,15 @@
 exports.handler = async function(event, context) {
-    const apiToken = process.env.API_TOKEN
+    const apiToken = process.env.API_TOKEN;
 
     fetch("/https://platform.smapone.com/Backend/v1/Smaps/6dc3a428-076f-4fd9-b257-c5cf94a78ed5/Data?accessToken=" + apiToken)
-    .then((response) => response.json())
-    .then((data) => {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                event: event,
-                context: context
-            })
-        }
-    })
+        .then((response) => response.json())
+        .then((data) => {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    event: event,
+                    context: context
+                })
+            };
+        });
 }
