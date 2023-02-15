186aaebf620cdfd9ae1be1d3f5b2cda57bc09084
Moritz <smappetizer@gmail.com>
2023-02-15 16:21:11 +0100

    Check if api token is set

--- netlify/functions/api-call.js
+++ netlify/functions/api-call.js
@@ -1,15 +1,16 @@
 exports.handler = async function(event, context) {
     const apiToken = process.env.API_TOKEN
+    return {
+        statusCode: 200,
+        body: JSON.stringify(apiToken)
+    }
 
-    fetch("https://platform.smapone.com/Backend/v1/Smaps/6dc3a428-076f-4fd9-b257-c5cf94a78ed5/Data?accessToken=" + apiToken)
-        .then((response) => response.json())
-        .then((data) => {
-            return {
-                statusCode: 200,
-                body: JSON.stringify({
-                    event: event,
-                    context: context
-                })
-            }
-        })
+    // fetch("https://platform.smapone.com/Backend/v1/Smaps/6dc3a428-076f-4fd9-b257-c5cf94a78ed5/Data?accessToken=" + apiToken)
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //         return {
+    //             statusCode: 200,
+    //             body: JSON.stringify(data)
+    //         }
+    //     })
 }
