ba638a9ffbed43031199c2477015903b80e32616
Moritz <smappetizer@gmail.com>
2023-02-15 16:25:41 +0100

    Use node-fetch

--- netlify/functions/api-call.js
+++ netlify/functions/api-call.js
@@ -1,16 +1,27 @@
-exports.handler = async function(event, context) {
-    const apiToken = process.env.API_TOKEN
-    return {
-        statusCode: 200,
-        body: JSON.stringify(apiToken)
+const fetch = require('node-fetch')
+
+const apiToken = process.env.API_TOKEN
+const API_ENDPOINT = 'https://platform.smapone.com/Backend/v1/Smaps/6dc3a428-076f-4fd9-b257-c5cf94a78ed5/Data?accessToken=' + apiToken
+
+exports.handler = async (event, context) => {
+    let response
+    try {
+        response = await fetch(API_ENDPOINT)
+    // Handle error
+    } catch (err) {
+        return {
+            statusCode: err.statusCode || 500,
+            body: JSON.stringify({
+                error: err.message
+            })
+        }
     }
 
-    // fetch("https://platform.smapone.com/Backend/v1/Smaps/6dc3a428-076f-4fd9-b257-c5cf94a78ed5/Data?accessToken=" + apiToken)
-    //     .then((response) => response.json())
-    //     .then((data) => {
-    //         return {
-    //             statusCode: 200,
-    //             body: JSON.stringify(data)
-    //         }
-    //     })
+    // Handle success
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            data: response
+        })
+    }
 }
