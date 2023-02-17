4aa7da04f7208361f49e9777f3b37eda19605f8b
Moritz <smappetizer@gmail.com>
2023-02-18 00:22:52 +0100

    Fix attempt

--- netlify/functions/api-call.js
+++ netlify/functions/api-call.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch'
+import {fetch} from 'node-fetch'
 
 const apiToken = process.env.API_TOKEN
 const API_ENDPOINT = 'https://platform.smapone.com/Backend/v1/Smaps/6dc3a428-076f-4fd9-b257-c5cf94a78ed5/Data?accessToken=' + apiToken
