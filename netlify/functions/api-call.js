0b37fcf947539848f3c9c1c50758f5f730ebbc9a
Moritz <smappetizer@gmail.com>
2023-02-20 09:49:33 +0100

    Final try

--- netlify/functions/api-call.js
+++ netlify/functions/api-call.js
@@ -1,42 +1,27 @@
-import fetch from 'node-fetch'
-
 const API_TOKEN = process.env.API_TOKEN
 const SMAP_ID = process.env.SMAP_ID
 const API_ENDPOINT = `https://platform.smapone.com/Backend/v1/Smaps/${SMAP_ID}/Data?accessToken=${API_TOKEN}`
-const VERSION = process.version
-
-console.log({VERSION})
 
 exports.handler = async (event, context) => {
+    let response
+    try {
+        response = await fetch(API_ENDPOINT)
+
+    // Handle error
+    } catch (err) {
+        return {
+            statusCode: err.statusCode || 500,
+            body: JSON.stringify({
+                error: err.message
+            })
+        }
+    }
+
+    // Handle success
     return {
         statusCode: 200,
         body: JSON.stringify({
-            "version": VERSION,
-            "endpoint": API_ENDPOINT,
-            "yes": true,
-            "fetch": fetch
+            data: response
         })
     }
-
-    // let response
-    // try {
-    //     response = await fetch(API_ENDPOINT)
-
-    // // Handle error
-    // } catch (err) {
-    //     return {
-    //         statusCode: err.statusCode || 500,
-    //         body: JSON.stringify({
-    //             error: err.message
-    //         })
-    //     }
-    // }
-
-    // // Handle success
-    // return {
-    //     statusCode: 200,
-    //     body: JSON.stringify({
-    //         data: response
-    //     })
-    // }
 }
