9414d997eb9752e695a502e30b9178df74dd42e9
Moritz <smappetizer@gmail.com>
2023-02-20 11:51:32 +0100

    Add display

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const app = document.getElementById("app")
+const dateOptions = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' }
 
-fetch("/.netlify/functions/api-call")
+// fetch("/.netlify/functions/api-call")
+fetch("data.json")
     .then((response) => response.json())
     .then((data) => {
-        console.log(data)
+        const app = document.getElementById("app")
+        let ui, li
+        ul = document.createElement("ul")
+        app.appendChild(ul)
+
+        data = data.reverse()
+        data.forEach(element => {
+            li = document.createElement("li")
+            let date = new Date(element.receivedDate)
+            
+            li.innerHTML = `
+                ID: <code>${element.id}</code><br>
+                User: ${element.userName}<br>
+                Datum: ${date.toLocaleDateString("de-DE", dateOptions)} Uhr<br>
+                Defekt: ${element.data.Maschine.Code} ${element.data.Maschine.Bezeichnung} [${element.data.Dringlichkeit}] ${element.data.Defektbeschreibung}
+            `
+            ul.appendChild(li)
+        })
     })
