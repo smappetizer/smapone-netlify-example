c58f9b461f0362cc4cc41a4f48c3087c43880846
Moritz <smappetizer@gmail.com>
2023-02-20 11:29:47 +0100

    Pass body directly... (II)

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const app = document.getElementById("app")
 fetch("/.netlify/functions/api-call")
     .then((response) => response.json())
     .then((data) => {
-        console.log(data.data)
+        console.log(data)
     })
