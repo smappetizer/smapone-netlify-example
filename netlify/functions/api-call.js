589792aba6ea373b48dc9b44624c2c2238261a94
Moritz <smappetizer@gmail.com>
2023-02-20 09:36:18 +0100

    Sync

--- netlify/functions/api-call.js
+++ netlify/functions/api-call.js
@@ -1,27 +1,36 @@
 const API_TOKEN = process.env.API_TOKEN
 const SMAP_ID = process.env.SMAP_ID
 const API_ENDPOINT = `https://platform.smapone.com/Backend/v1/Smaps/${SMAP_ID}/Data?accessToken=${API_TOKEN}`
+const VERSION = process.version
 
 exports.handler = async (event, context) => {
-    let response
-    try {
-        response = await fetch(API_ENDPOINT)
-
-    // Handle error
-    } catch (err) {
-        return {
-            statusCode: err.statusCode || 500,
-            body: JSON.stringify({
-                error: err.message
-            })
-        }
-    }
-
-    // Handle success
     return {
         statusCode: 200,
         body: JSON.stringify({
-            data: response
+            version: VERSION,
+            endpoint: API_ENDPOINT
         })
     }
+
+    // let response
+    // try {
+    //     response = await fetch(API_ENDPOINT)
+
+    // // Handle error
+    // } catch (err) {
+    //     return {
+    //         statusCode: err.statusCode || 500,
+    //         body: JSON.stringify({
+    //             error: err.message
+    //         })
+    //     }
+    // }
+
+    // // Handle success
+    // return {
+    //     statusCode: 200,
+    //     body: JSON.stringify({
+    //         data: response
+    //     })
+    // }
 }
