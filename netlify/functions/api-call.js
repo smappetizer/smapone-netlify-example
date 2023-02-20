b10d6890743a00215e1cd073826c960754d9234e
Moritz <smappetizer@gmail.com>
2023-02-20 09:33:45 +0100

    Update

--- netlify/functions/api-call.js
+++ netlify/functions/api-call.js
@@ -1,32 +1,27 @@
 const API_TOKEN = process.env.API_TOKEN
-const API_ENDPOINT = 'https://platform.smapone.com/Backend/v1/Smaps/6dc3a428-076f-4fd9-b257-c5cf94a78ed5/Data?accessToken=' + API_TOKEN
+const SMAP_ID = process.env.SMAP_ID
+const API_ENDPOINT = `https://platform.smapone.com/Backend/v1/Smaps/${SMAP_ID}/Data?accessToken=${API_TOKEN}`
 
 exports.handler = async (event, context) => {
+    let response
+    try {
+        response = await fetch(API_ENDPOINT)
+
+    // Handle error
+    } catch (err) {
+        return {
+            statusCode: err.statusCode || 500,
+            body: JSON.stringify({
+                error: err.message
+            })
+        }
+    }
+
+    // Handle success
     return {
         statusCode: 200,
         body: JSON.stringify({
-            token: API_TOKEN,
-            endpoint: API_ENDPOINT
+            data: response
         })
     }
-    // let response
-    // try {
-    //     response = await fetch(API_ENDPOINT)
-    // // Handle error
-    // } catch (err) {
-    //     return {
-    //         statusCode: err.statusCode || 500,
-    //         body: JSON.stringify({
-    //             error: err.message
-    //         })
-    //     }
-    // }
-
-    // // Handle success
-    // return {
-    //     statusCode: 200,
-    //     body: JSON.stringify({
-    //         data: response
-    //     })
-    // }
 }
